06ffb841f15edd77d558c66fb6f6c2d175755911
dariocavada <dario.cavada.lab@gmail.com>
2023-08-24 11:39:21 +0200

    PanoramaViewer 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# panorama_viewer
-Panorama Viewer - Flutter Widget
+# Panorama Viewer
+
+A 360-degree panorama viewer.
+
+This package is an updated porting of the plugin https://github.com/zesage/panorama.
+
+## Getting Started
+
+Add panorama as a dependency in your pubspec.yaml file.
+
+```yaml
+dependencies:
+  panorama_viewer: ^1.0.1
+```
+
+Import and add the Panorama Viewer widget to your project.
+
+```dart
+import 'package:panorama_viewer/panorama_viewer.dart';
+... ...
+  
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: PanoramaViewer(
+          child: Image.asset('assets/panorama360.jpg'),
+        ),
+      ),
+    );
+  }
+```
+
+## Migration from the Panorama package
+
+- In the dependencies, use `panorama_viewer` instead of `panorama`.
+- In the Dart files where you use panorama, change the import to: `import 'package:panorama_viewer/panorama_viewer.dart';`.
+- Change the widget name from `Panorama` to `PanoramaViewer`.
+- If you've used `SensorControl`, change `SensorControl.Orientation` to `SensorControl.orientation`. All constant names are now in lower camel case, following the latest Dart best practices.
+
+
+
+
+
+
+
---
+++ example/lib/main.dart
@@ -0,0 +1,191 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:panorama_viewer/panorama_viewer.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Panorama',
+      theme: ThemeData.dark(),
+      home: const MyHomePage(title: 'Flutter Panorama Viewer'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  double _lon = 0;
+  double _lat = 0;
+  double _tilt = 0;
+  int _panoId = 0;
+  List<Image> panoImages = [
+    Image.asset('assets/panorama1.webp'),
+    Image.asset('assets/panorama2.webp'),
+    Image.asset('assets/panorama3.webp'),
+  ];
+
+  void onViewChanged(longitude, latitude, tilt) {
+    setState(() {
+      _lon = longitude;
+      _lat = latitude;
+      _tilt = tilt;
+    });
+  }
+
+  Widget hotspotButton(
+      {String? text, IconData? icon, VoidCallback? onPressed}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(const CircleBorder()),
+            backgroundColor: MaterialStateProperty.all(Colors.black38),
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+          ),
+          onPressed: onPressed,
+          child: Icon(icon),
+        ),
+        text != null
+            ? Container(
+                padding: const EdgeInsets.all(4.0),
+                decoration: const BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.all(Radius.circular(4))),
+                child: Center(child: Text(text)),
+              )
+            : Container(),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget panorama;
+    switch (_panoId % panoImages.length) {
+      case 0:
+        panorama = PanoramaViewer(
+          animSpeed: .0,
+          sensorControl: SensorControl.orientation,
+          onViewChanged: onViewChanged,
+          onTap: (longitude, latitude, tilt) =>
+              print('onTap: $longitude, $latitude, $tilt'),
+          onLongPressStart: (longitude, latitude, tilt) =>
+              print('onLongPressStart: $longitude, $latitude, $tilt'),
+          onLongPressMoveUpdate: (longitude, latitude, tilt) =>
+              print('onLongPressMoveUpdate: $longitude, $latitude, $tilt'),
+          onLongPressEnd: (longitude, latitude, tilt) =>
+              print('onLongPressEnd: $longitude, $latitude, $tilt'),
+          hotspots: [
+            Hotspot(
+              latitude: -15.0,
+              longitude: -129.0,
+              width: 90,
+              height: 75,
+              widget: hotspotButton(
+                  text: "Next scene",
+                  icon: Icons.open_in_browser,
+                  onPressed: () => setState(() => _panoId++)),
+            ),
+            Hotspot(
+              latitude: -42.0,
+              longitude: -46.0,
+              width: 60.0,
+              height: 60.0,
+              widget: hotspotButton(
+                  icon: Icons.search,
+                  onPressed: () => setState(() => _panoId = 2)),
+            ),
+            Hotspot(
+              latitude: -33.0,
+              longitude: 123.0,
+              width: 60.0,
+              height: 60.0,
+              widget: hotspotButton(icon: Icons.arrow_upward, onPressed: () {}),
+            ),
+          ],
+          child: Image.asset('assets/panorama1.webp'),
+        );
+        break;
+      case 2:
+        panorama = PanoramaViewer(
+          animSpeed: 0.0,
+          sensorControl: SensorControl.orientation,
+          onViewChanged: onViewChanged,
+          /*croppedArea: const Rect.fromLTWH(2533.0, 1265.0, 5065.0, 2533.0),
+          croppedFullWidth: 10132.0,
+          croppedFullHeight: 5066.0,*/
+          hotspots: [
+            Hotspot(
+              latitude: 0.0,
+              longitude: -46.0,
+              width: 90.0,
+              height: 75.0,
+              widget: hotspotButton(
+                  text: "Next scene",
+                  icon: Icons.double_arrow,
+                  onPressed: () => setState(() {
+                        _panoId++;
+                        if (_panoId > panoImages.length - 1) {
+                          _panoId = 0;
+                        }
+                      })),
+            ),
+          ],
+          child: Image.asset('assets/panorama3.webp'),
+        );
+        break;
+      default:
+        panorama = PanoramaViewer(
+          animSpeed: 0.0,
+          sensorControl: SensorControl.orientation,
+          onViewChanged: onViewChanged,
+          hotspots: [
+            Hotspot(
+              latitude: 0.0,
+              longitude: 160.0,
+              width: 90.0,
+              height: 75.0,
+              widget: hotspotButton(
+                  text: "Next scene",
+                  icon: Icons.double_arrow,
+                  onPressed: () => setState(() {
+                        _panoId++;
+                        if (_panoId > panoImages.length - 1) {
+                          _panoId = 0;
+                        }
+                      })),
+            ),
+          ],
+          child: panoImages[_panoId],
+        );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+      ),
+      body: Stack(
+        children: [
+          panorama,
+          /*Text(
+            '${_lon.toStringAsFixed(3)}, ${_lat.toStringAsFixed(3)}, ${_tilt.toStringAsFixed(3)}',
+          ),*/
+        ],
+      ),
+    );
+  }
+}
